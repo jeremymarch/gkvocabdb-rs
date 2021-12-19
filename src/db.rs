cba1d8ffafe8e74432e388f26e3f7420646b1073
jeremymarch <jmarch@gradcenter.cuny.edu>
2021-12-19 17:59:02 -0500

    First commit.  First test passes.

---
+++ src/db.rs
@@ -0,0 +1,199 @@
+/*
+gkvocabdb
+
+Copyright (C) 2021  Jeremy March
+
+This program is free software: you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation, either version 3 of the License, or
+(at your option) any later version.
+
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with this program.  If not, see <https://www.gnu.org/licenses/>. 
+*/
+
+use sqlx::sqlite::SqliteRow;
+use sqlx::{FromRow, Row, SqlitePool };
+use serde::{Deserialize, Serialize};
+/*
+#[derive(Debug, Serialize, Deserialize, Clone)]
+pub enum PhilologusWords {
+    GreekDefs { seq: u32, def: String },
+}
+
+#[derive(Debug, Serialize, Deserialize, Clone, FromRow)]
+pub struct DefRow {
+    pub word: String,
+    pub sortword: String,
+    pub def: String,
+    pub seq: u32
+}
+*/
+
+#[derive(Debug, Serialize, Deserialize, Clone, FromRow)]
+pub struct WordRow {
+    pub wordid:u32,
+    pub word:String,
+    pub word_type:u8,
+    pub lemma:String,
+    pub lemma1:String,
+    pub def:String,
+    pub unit:u8,
+    pub pos:String,
+    pub arrowed_id:u32,
+    pub hqid:u32,
+    pub seq:u32,
+    pub arrowed_seq: u32,
+    pub freq: u32, 
+    pub runningcount: u32,
+    pub is_flagged: bool
+}
+
+pub async fn get_words(pool: &SqlitePool, _textid:u32) -> Result<Vec<WordRow>, sqlx::Error> {
+
+    let query = format!("SELECT A.wordid,A.word,A.type,B.lemma,A.lemma1,B.def,B.unit,pos,B.arrowedID,B.hqid,A.seq,C.seq AS arrowedSeq, \
+    B.freq, A.runningcount,A.isFlagged \
+    FROM gkvocabdb A \
+    LEFT JOIN hqvocab B ON A.lemmaid = B.hqid \
+    LEFT JOIN gkvocabdb C on B.arrowedID = C.wordid \
+    WHERE A.seq >= {start} AND A.seq <= {end} AND A.type > -1 \
+    ORDER BY A.seq \
+    LIMIT 55000;", start=0,end=1000);
+
+    let res: Result<Vec<WordRow>, sqlx::Error> = sqlx::query(&query)
+    .map(|rec: SqliteRow| 
+        WordRow {
+            wordid: rec.get("wordid"),
+            word: rec.get("word"),
+            word_type: rec.get("type"),
+            lemma: rec.get("lemma"),
+            lemma1: rec.get("lemma1"),
+            def: rec.get("def"),
+            unit: rec.get("unit"),
+            pos: rec.get("pos"),
+            arrowed_id: rec.get("arrowedID"),
+            hqid: rec.get("hqid"),
+            seq: rec.get("seq"),
+            arrowed_seq: rec.get("arrowedSeq"),
+            freq: rec.get("freq"), 
+            runningcount: rec.get("runningcount"),
+            is_flagged: rec.get("isFlagged")
+        }    
+    )
+    .fetch_all(pool)
+    .await;
+
+    res
+}
+
+pub async fn get_titles(pool: &SqlitePool) -> Result<Vec<(String,u32)>, sqlx::Error> {
+    let query = format!("SELECT id,title FROM titles ORDER BY title;");
+    let res: Result<Vec<(String,u32)>, sqlx::Error> = sqlx::query(&query)
+    .map(|rec: SqliteRow| (rec.get("id"),rec.get("title")) )
+    .fetch_all(pool)
+    .await;
+
+    res
+}
+
+/*
+CREATE TABLE IF NOT EXISTS sequences (seq_id INTEGER PRIMARY KEY AUTOINCREMENT NOT NULL, name text NOT NULL);
+CREATE TABLE IF NOT EXISTS text (text_id INTEGER PRIMARY KEY AUTOINCREMENT NOT NULL, name text NOT NULL);
+CREATE TABLE IF NOT EXISTS sequence_x_text (seq_id INTEGER NOT NULL, text_id INTEGER NOT NULL;
+CREATE TABLE sqlite_sequence(name,seq);
+CREATE TABLE IF NOT EXISTS words (word_id INTEGER PRIMARY KEY AUTOINCREMENT NOT NULL, text_id INTEGER NOT NULL, sequence_in_text INTEGER NOT NULL, word ΤΕΧΤ NOT NULL...)
+
+.mode ascii
+.separator "," "\n"
+.import /Users/jeremy/Downloads/gkvocabdbxxx.csv gkvocabdb
+
+//https://github.com/dumblob/mysql2sqlite
+mysqldump --skip-extended-insert --compact philolog_us gkvocabdb hqvocab gkvocabAssignments appcrit > gkvocabdbxxxx.sql
+./mysql2sqlite gkvocabdbxxxx.sql | sqlite3 gkvocabdb2.sqlite
+
+  CREATE TABLE IF NOT EXISTS gkvocabdb (
+    wordid INTEGER PRIMARY KEY AUTOINCREMENT NOT NULL,
+    seq int(10) NOT NULL,
+    text int(11) NOT NULL,
+    section varchar(255) DEFAULT NULL,
+    line varchar(255)  DEFAULT NULL,
+    word varchar(255)  NOT NULL,
+    lemmaid int(10)  DEFAULT NULL,
+    lemmaa varchar(255)  NOT NULL,
+    lemmab varchar(255)  NOT NULL,
+    o varchar(255)  NOT NULL,
+    runningcount int(10)  NOT NULL,
+    type tinyint(4) DEFAULT NULL,
+    arrow tinyint(1) NOT NULL DEFAULT 0,
+    flagged tinyint(1) NOT NULL DEFAULT 0,
+    updated timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    updatedUserAgent varchar(255)  NOT NULL DEFAULT '',
+    updatedIP varchar(255)  NOT NULL DEFAULT '',
+    updatedUser varchar(255)  NOT NULL DEFAULT '',
+    isFlagged tinyint(4) NOT NULL DEFAULT 0,
+    note varchar(1024)  NOT NULL DEFAULT ''
+  )
+    CREATE TABLE gkvocabdb (wordid INTEGER PRIMARY KEY AUTOINCREMENT NOT NULL,seq int(10) NOT NULL,text int(11) NOT NULL,section varchar(255) DEFAULT NULL,line varchar(255)  DEFAULT NULL,word varchar(255)  NOT NULL,lemmaid int(10)  DEFAULT NULL,lemma1 varchar(255) NOT NULL,lemma2 varchar(255)  NOT NULL,o varchar(255)  NOT NULL,runningcount int(10)  NOT NULL,type tinyint(4) DEFAULT NULL,arrow tinyint(1) NOT NULL DEFAULT 0,flagged tinyint(1) NOT NULL DEFAULT 0,updated timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP,updatedUserAgent varchar(255)  NOT NULL DEFAULT '',updatedIP varchar(255)  NOT NULL DEFAULT '',updatedUser varchar(255)  NOT NULL DEFAULT '',isFlagged tinyint(4) NOT NULL DEFAULT 0,note varchar(1024)  NOT NULL DEFAULT '',KEY lemma1 (lemma1),KEY seq (seq),KEY lemmaid (lemmaid))
+
+  CREATE TABLE hqvocab (
+    hqid int(11)  NOT NULL AUTOINCREMENT,
+    seqold smallint(5)  NOT NULL DEFAULT 0,
+    seq smallint(10)  NOT NULL DEFAULT 0,
+    unit smallint(6)  NOT NULL,
+    lemma varchar(256) NOT NULL,
+    lemma2 varchar(255) NOT NULL DEFAULT '',
+    sortalpha varchar(255)  NOT NULL DEFAULT '',
+    sortkey varchar(255)  NOT NULL,
+    present varchar(256)  NOT NULL,
+    future varchar(256)  NOT NULL,
+    aorist varchar(256)  NOT NULL,
+    perfect varchar(256)  NOT NULL,
+    perfectmid varchar(256)  NOT NULL,
+    aoristpass varchar(256)  NOT NULL,
+    def varchar(1024)  NOT NULL,
+    pos varchar(256)  NOT NULL,
+    link varchar(256)  NOT NULL,
+    freq smallint(6) NOT NULL,
+    note varchar(256)  NOT NULL,
+    verbClass int(10)  NOT NULL DEFAULT 0,
+    updated timestamp NOT NULL DEFAULT current_timestamp() ON UPDATE current_timestamp(),
+    arrowedDay smallint(5)  DEFAULT NULL,
+    arrowedID int(11) DEFAULT NULL,
+    pageLine varchar(255)  DEFAULT NULL,
+    parentid int(11)  DEFAULT NULL,
+    status tinyint(4) NOT NULL DEFAULT 1,
+    updatedUserAgent varchar(255)  NOT NULL DEFAULT '',
+    updatedIP varchar(255)  NOT NULL DEFAULT '',
+    updatedUser varchar(255)  NOT NULL DEFAULT '',
+    PRIMARY KEY (hqid),
+    KEY updated (updated),
+    KEY seq (seqold),
+    KEY sortkey (sortkey),
+    KEY sortalpha (sortalpha),
+    KEY lemma (lemma(255)),
+    KEY parentididx (parentid)
+  )
+
+  CREATE TABLE gkvocabAssignments (
+    id int(11) NOT NULL AUTO_INCREMENT,
+    sort int(11) NOT NULL,
+    title varchar(255) NOT NULL,
+    start int(10)  NOT NULL,
+    end int(10)  NOT NULL,
+    wordcount int(10)  DEFAULT NULL,
+    PRIMARY KEY (id)
+  )
+
+  CREATE TABLE appcrit (
+    wordid int(10)  NOT NULL,
+    entry varchar(1024) DEFAULT NULL,
+    PRIMARY KEY (wordid)
+  )
+*/
+
+
---
+++ src/main.rs
@@ -0,0 +1,268 @@
+/*
+gkvocabdb
+
+Copyright (C) 2021  Jeremy March
+
+This program is free software: you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation, either version 3 of the License, or
+(at your option) any later version.
+
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with this program.  If not, see <https://www.gnu.org/licenses/>. 
+*/
+use thiserror::Error;
+use actix_web::{ ResponseError, http::StatusCode};
+use percent_encoding::percent_decode_str;
+
+use std::io;
+use regex::Regex;
+use actix_files as fs;
+use actix_web::{middleware, web, App, Error as AWError, HttpResponse, HttpRequest, HttpServer, Result};
+use sqlx::SqlitePool;
+
+use actix_files::NamedFile;
+use std::path::PathBuf;
+
+mod db;
+use crate::db::*;
+use serde::{Deserialize, Serialize};
+
+use std::time::{SystemTime, UNIX_EPOCH};
+
+//https://stackoverflow.com/questions/64348528/how-can-i-pass-multi-variable-by-actix-web-appdata
+//https://doc.rust-lang.org/rust-by-example/generics/new_types.html
+#[derive(Clone)]
+struct SqliteUpdatePool (SqlitePool);
+
+
+#[derive(Debug, Serialize, Deserialize, Clone)]
+struct QueryResponse {
+    #[serde(rename(serialize = "thisText"), rename(deserialize = "thisText"))]
+    this_text: u32,
+    words: Vec<WordRow>,
+    #[serde(rename(serialize = "selectedid"), rename(deserialize = "selectedid"))]
+    selected_id: u32,
+    error: String,
+}
+
+#[derive(Deserialize)]
+pub struct QueryRequest {
+    pub text: u32,
+    pub wordid: u32,
+}
+/*
+#[derive(Deserialize)]
+pub struct WordQuery {
+    pub regex: String,
+    pub lexicon: String,
+    pub tag_id: String,
+    pub root_id: String,
+    pub wordid: Option<String>,
+    pub w: String,
+}
+*/
+#[allow(clippy::eval_order_dependence)]
+async fn philologus_words((info, req): (web::Query<QueryRequest>, HttpRequest)) -> Result<HttpResponse, AWError> {
+    let db = req.app_data::<SqlitePool>().unwrap();
+
+    //let query_params: WordQuery = serde_json::from_str(&info.query)?;
+
+    let w = get_words(db, info.text).await.map_err(map_sqlx_error)?;
+
+/*
+    $j = new \stdClass();
+    if ($words == "WordAssignmentError" ) {
+        $j->error = "Error getting text assignments.";
+    }
+    $j->thisText = $textid;
+    $j->words = $words;
+    $j->selectedid = $selectedid;
+*/
+
+//{"thisText":"1","words":[{"i":"1","w":"530a","t":"4","l":null,"pos":null,"l1":"","def":null,"u":null,"a":null,"hqid":null,"s":"1","s2":null,"c":null,"rc":"0","if":"0"},
+//{"i":"2","w":"ΣΩ.","t":"2","l":null,"pos":null,"l1":"","def":null,"u":null,"a":null,"hqid":null,"s":"2","s2":null,"c":null,"rc":"0","if":"0"}],"selectedid":0}
+ 
+    let res = QueryResponse {
+        this_text: 1,
+        words: w,
+        selected_id: 1,
+        error: "".to_string(),
+    };
+
+    Ok(HttpResponse::Ok().json(res))
+}
+
+fn get_user_agent(req: &HttpRequest) -> Option<&str> {
+    req.headers().get("user-agent")?.to_str().ok()
+}
+
+async fn health_check(_req: HttpRequest) -> Result<HttpResponse, AWError> {
+    Ok(HttpResponse::Ok().finish()) //send 200 with empty body
+}
+
+#[actix_web::main]
+async fn main() -> io::Result<()> {
+    std::env::set_var("RUST_LOG", "actix_web=info");
+    env_logger::init();
+
+    //e.g. export GKVOCABDB_DB_PATH=sqlite://gkvocabdb.sqlite?mode=rwc
+    let db_path = std::env::var("GKVOCABDB_DB_PATH")
+                   .unwrap_or_else(|_| panic!("Environment variable for sqlite path not set: GKVOCABDB_DB_PATH."));
+    let db_pool = SqlitePool::connect(&db_path).await.expect("Could not connect to db.");
+
+    /*
+    https://github.com/SergioBenitez/Rocket/discussions/1989
+    .journal_mode(SqliteJournalMode::Off)
+    .read_only(true)
+    */
+/*
+    let error_handlers = ErrorHandlers::new()
+            .handler(
+                http::StatusCode::INTERNAL_SERVER_ERROR,
+                api::internal_server_error,
+            )
+            .handler(http::StatusCode::BAD_REQUEST, api::bad_request)
+            .handler(http::StatusCode::NOT_FOUND, api::not_found);
+*/
+    HttpServer::new(move || {
+        App::new()
+            .app_data(db_pool.clone())
+            .wrap(middleware::Logger::default())
+            .wrap(middleware::Compress::default())
+            //.wrap(error_handlers)
+            .service(
+                web::resource("/query")
+                    .route(web::get().to(philologus_words)),
+            )
+            .service(
+                web::resource("/healthzzz")
+                    .route(web::get().to(health_check)),
+            )
+            .service(fs::Files::new("/", "./static").prefer_utf8(true).index_file("index.html"))
+    })
+    .bind("0.0.0.0:8088")?
+    .run()
+    .await
+}
+
+#[derive(Error, Debug)]
+pub struct PhilologusError {
+       code: StatusCode,
+       name: String,
+       error: String,
+}
+
+impl std::fmt::Display for PhilologusError {
+    fn fmt(&self, fmt: &mut std::fmt::Formatter<'_>) -> Result<(), std::fmt::Error> {
+        write!(fmt, "PhilologusError: {} {}: {}.", self.code.as_u16(), self.name, self.error)
+    }
+}
+
+impl ResponseError for PhilologusError {
+    fn error_response(&self) -> HttpResponse {
+        let error_response = ErrorResponse {
+            code: self.code.as_u16(),
+            message: self.error.to_string(),
+            error: self.name.to_string(),
+        };
+        HttpResponse::build(self.code).json(error_response)
+    }
+}
+
+fn map_sqlx_error(e: sqlx::Error) -> PhilologusError {   
+    match e {
+        sqlx::Error::Configuration(e) => PhilologusError { code: StatusCode::INTERNAL_SERVER_ERROR, name: "sqlx error".to_string(), error: format!("sqlx Configuration: {}", e) },
+        sqlx::Error::Database(e) => PhilologusError { code: StatusCode::INTERNAL_SERVER_ERROR, name: "sqlx error".to_string(), error: format!("sqlx Database: {}", e) },
+        sqlx::Error::Io(e) => PhilologusError { code: StatusCode::INTERNAL_SERVER_ERROR, name: "sqlx error".to_string(), error: format!("sqlx Io: {}", e) },
+        sqlx::Error::Tls(e) => PhilologusError { code: StatusCode::INTERNAL_SERVER_ERROR, name: "sqlx error".to_string(), error: format!("sqlx Tls: {}", e) },
+        sqlx::Error::Protocol(e) => PhilologusError { code: StatusCode::INTERNAL_SERVER_ERROR, name: "sqlx error".to_string(), error: format!("sqlx Protocol: {}", e) },
+        sqlx::Error::RowNotFound => PhilologusError { code: StatusCode::INTERNAL_SERVER_ERROR, name: "sqlx error".to_string(), error: "sqlx RowNotFound".to_string() },
+        sqlx::Error::TypeNotFound { .. } => PhilologusError { code: StatusCode::INTERNAL_SERVER_ERROR, name: "sqlx error".to_string(), error: "sqlx TypeNotFound".to_string() },
+        sqlx::Error::ColumnIndexOutOfBounds { .. } => PhilologusError { code: StatusCode::INTERNAL_SERVER_ERROR, name: "sqlx error".to_string(), error: "sqlx ColumnIndexOutOfBounds".to_string() },
+        sqlx::Error::ColumnNotFound(e) => PhilologusError { code: StatusCode::INTERNAL_SERVER_ERROR, name: "sqlx error".to_string(), error: format!("sqlx ColumnNotFound: {}", e) },
+        sqlx::Error::ColumnDecode { .. } => PhilologusError { code: StatusCode::INTERNAL_SERVER_ERROR, name: "sqlx error".to_string(), error: "sqlx ColumnDecode".to_string() },
+        sqlx::Error::Decode(e) => PhilologusError { code: StatusCode::INTERNAL_SERVER_ERROR, name: "sqlx error".to_string(), error: format!("sqlx Decode: {}", e) },
+        sqlx::Error::PoolTimedOut => PhilologusError { code: StatusCode::INTERNAL_SERVER_ERROR, name: "sqlx error".to_string(), error: "sqlx PoolTimeOut".to_string() },
+        sqlx::Error::PoolClosed => PhilologusError { code: StatusCode::INTERNAL_SERVER_ERROR, name: "sqlx error".to_string(), error: "sqlx PoolClosed".to_string() },
+        sqlx::Error::WorkerCrashed => PhilologusError { code: StatusCode::INTERNAL_SERVER_ERROR, name: "sqlx error".to_string(), error: "sqlx WorkerCrashed".to_string() },
+        sqlx::Error::Migrate(e) => PhilologusError { code: StatusCode::INTERNAL_SERVER_ERROR, name: "sqlx error".to_string(), error: format!("sqlx Migrate: {}", e) },
+        _ => PhilologusError { code: StatusCode::INTERNAL_SERVER_ERROR, name: "sqlx error".to_string(), error: "sqlx Unknown error".to_string() },
+    }
+}
+
+#[derive(Serialize)]
+struct ErrorResponse {
+    code: u16,
+    error: String,
+    message: String,
+}
+
+fn map_utf8_error(_e: std::str::Utf8Error) -> PhilologusError {   
+    PhilologusError { code: StatusCode::INTERNAL_SERVER_ERROR, name: "percent_decode_str utf-8 error".to_string(), error: "percent_decode_str utf-8 error".to_string() }
+}
+
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+    use actix_web::{test, web, App};
+
+    //use serde::{Serialize, Deserialize};
+    //use percent_encoding::{utf8_percent_encode, NON_ALPHANUMERIC};
+    //use actix_web::http::header::ContentType;
+/*
+    #[actix_rt::test]
+    async fn test_index_get() {
+        let mut app = test::init_service(App::new().route("/", web::get().to(index))).await;
+        let req = test::TestRequest::with_header("content-type", "text/plain").to_request();
+        let resp = test::call_service(&mut app, req).await;
+        assert!(resp.status().is_success());
+    }
+
+    #[actix_rt::test]
+    async fn test_index_post() {
+        let mut app = test::init_service(App::new().route("/", web::get().to(index))).await;
+        let req = test::TestRequest::post().uri("/").to_request();
+        let resp = test::call_service(&mut app, req).await;
+        assert!(resp.status().is_client_error());
+    }
+*/
+
+    //cargo test -- --nocapture
+
+    #[actix_web::test]
+    async fn test_query_paging() {
+        let db_path = std::env::var("GKVOCABDB_DB_PATH")
+            .unwrap_or_else(|_| panic!("Environment variable for sqlite path not set: GKVOCABDB_DB_PATH."));
+
+        let db_pool = SqlitePool::connect(&db_path).await.expect("Could not connect to db.");
+
+        let mut app = test::init_service(
+            App::new()
+            .app_data(db_pool.clone())
+            .service(
+                web::resource("/healthzzz")
+                    .route(web::get().to(health_check)),
+            )
+            .service(
+                web::resource("/query")
+                    .route(web::get().to(philologus_words)),
+        )).await;
+
+        let resp = test::TestRequest::get()
+        .uri(r#"/query?text=1&wordid=0"#) //400 Bad Request error if all params not present
+        .send_request(&mut app).await;
+
+        assert!(&resp.status().is_success());
+        //println!("resp: {:?}", resp);
+        let result: QueryResponse = test::read_body_json(resp).await;
+        //println!("res: {:?}", result);
+        assert_eq!(result.words.len(), 1000);
+    }
+}
