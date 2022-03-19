0c72ebd4276fea6121096c42c28304b381131c55
jeremymarch <jmarch@gradcenter.cuny.edu>
2022-03-19 01:33:47 -0400

    finish updating license to AGPL

--- src/login.rs
+++ src/login.rs
@@ -4,7 +4,7 @@ gkvocabdb
 Copyright (C) 2021  Jeremy March
 
 This program is free software: you can redistribute it and/or modify
-it under the terms of the GNU General Public License as published by
+it under the terms of the GNU Affero General Public License as published by
 the Free Software Foundation, either version 3 of the License, or
 (at your option) any later version.
 
