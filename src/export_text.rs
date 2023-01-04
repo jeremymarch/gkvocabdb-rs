6d80bd04f41d1c3c23c83d9285a4bbd6005a8d77
jeremymarch <jmarch@gradcenter.cuny.edu>
2023-01-03 21:50:00 -0500

    fix latex: now need to fix where latex document header is attached

--- src/export_text.rs
+++ src/export_text.rs
@@ -19,6 +19,7 @@ along with this program.  If not, see <https://www.gnu.org/licenses/>.
 
 use super::*;
 use std::collections::HashMap;
+//use actix_web::http::header::{ContentDisposition, ContentType};
 
 #[derive(Debug, Serialize, Deserialize, Clone,Eq,PartialEq)]
 struct Gloss {
@@ -100,10 +101,8 @@ pub async fn export_text(
             //last_seq = w.seq as i64;
     	    //last_word_id = w.wordid as i64;
 
-
             if !w.lemma.is_empty() && !w.def.is_empty() && !glosses.contains_key(&w.hqid) {
-
-                let is_arrowed;
+ 
                 // if (!is_null($row["arrowedSeq"]) && (int)$row["seq"] > (int)$row["arrowedSeq"]) {
                 //     //echo $row["seq"] . ", " . $row["arrowedSeq"] . "\n";
                 //     continue; //skip
@@ -116,6 +115,7 @@ pub async fn export_text(
                 //     $g->arrow = FALSE;
                 // }
 
+                let is_arrowed;
                 if w.arrowed_text_seq == Some(w.word_text_seq) && w.arrowed_seq == Some(w.seq) {
                     
                     is_arrowed = true; //this instance is arrowed
@@ -146,10 +146,14 @@ pub async fn export_text(
         let mut sorted_glosses:Vec<Gloss> = glosses.values().cloned().collect();
         sorted_glosses.sort_by(|a, b| a.sort_alpha.cmp(&b.sort_alpha));
 
-        let res = mapply_templates(&title, &mut res, &sorted_glosses, &header);
+        let mut res = apply_latex_templates(&title, &mut res, &sorted_glosses, &header);
+        res.push_str("\\end{document}\n");
 
+        //https://www.reddit.com/r/rust/comments/xzrznn/comment/irojs9f/?utm_source=share&utm_medium=web2x&context=3
+        //let filename = "export.tex";
         Ok(HttpResponse::Ok()
             .content_type("application/x-latex")
+            //.insert_header(ContentDisposition::Attachment(filename))
             .body(res))
     } else {
         let res = ImportResponse {
@@ -161,46 +165,46 @@ pub async fn export_text(
     }
 }
 
-fn mapply_templates(title:&str, text:&mut String, glosses:&Vec<Gloss>, header:&str) -> String {
+fn apply_latex_templates(title:&str, text:&mut String, glosses:&Vec<Gloss>, header:&str) -> String {
     let mut latex = String::from("");
     
-    latex.push_str(r"\newpage\n");
+    latex.push_str("\\newpage\n");
 
     if !header.is_empty() {
         if !title.is_empty() {
-            latex.push_str(r"\fancyhead[ER]{ }\n");
+            latex.push_str("\\fancyhead[ER]{ }\n");
         }
         else {
-            latex.push_str(format!(r"\fancyhead[ER]{{{}}}\n", header).as_str());
+            latex.push_str(format!("\\fancyhead[ER]{{{}}}\n", header).as_str());
         }
     }
     if !title.is_empty() {
-        latex.push_str(format!(r"\begin{{center}}\noindent\textbf{{{}}}\par\end{{center}}\n", title).as_str());
+        latex.push_str(format!("\\begin{{center}}\\noindent\\textbf{{{}}}\\par\\end{{center}}\n", title).as_str());
     }
 
-    if text.contains(r"%VERSELINESTART%") {
-        *text = text.replace(r"%StartSubTitle%", "");
-        *text = text.replace(r"%EndSubTitle%", " & "); //add this even if no %StartSubTitle%
-        *text = text.replace(r"%LINEEND%", " \\\\ \n");
-        *text = text.replace(r"%VERSELINESTART%", " & ");
-        *text = text.replace(r"%VERSELINEEND%", " \\\\ \n");
-        latex.push_str(format!(r"{}{}\end{{tabular}}\n", include_str!("latex/verse_table_start.tex"), text).as_str());
+    if text.contains("%VERSELINESTART%") {
+        *text = text.replace("%StartSubTitle%", "");
+        *text = text.replace("%EndSubTitle%", " & "); //add this even if no %StartSubTitle%
+        *text = text.replace("%LINEEND%", " \\\\ \n");
+        *text = text.replace("%VERSELINESTART%", " & ");
+        *text = text.replace("%VERSELINEEND%", " \\\\ \n");
+        latex.push_str(format!("{}{}\\end{{tabular}}\n", include_str!("latex/verse_table_start.tex"), text).as_str());
     }
     else {
 
-        latex.push_str(r"\begin{spacing}{\GlossLineSpacing}\n");
-        latex.push_str(r"\noindent\n");
+        latex.push_str("\\begin{spacing}{\\GlossLineSpacing}\n");
+        latex.push_str("\\noindent\n");
 
-        *text = text.replace(r"%StartSubTitle%", r"\begin{center}"); //\hspace{0pt} solves problem when \par\marginsec{} come together
-        *text = text.replace(r"%EndSubTitle%", r"\end{center}");
+        *text = text.replace("%StartSubTitle%", "\\begin{center}"); //\hspace{0pt} solves problem when \par\marginsec{} come together
+        *text = text.replace("%EndSubTitle%", "\\end{center}");
 
-        *text = text.replace(r"%StartSubSection%", r"\hspace{0pt}\marginsec{"); //\hspace{0pt} solves problem when \par\marginsec{} come together
-        *text = text.replace(r"%EndSubSection%", "}");
-        *text = text.replace(r"%StartSubSubSection%", r"\hspace{0pt}\marginseclight{"); //\hspace{0pt} solves problem when \par\marginsec{} come together
-        *text = text.replace(r"%EndSubSubSection%", "}");
-        *text = text.replace(r"%para%", "\n\\par\n");
+        *text = text.replace("%StartSubSection%", "\\hspace{0pt}\\marginsec{"); //\hspace{0pt} solves problem when \par\marginsec{} come together
+        *text = text.replace("%EndSubSection%", "}");
+        *text = text.replace("%StartSubSubSection%", "\\hspace{0pt}\\marginseclight{"); //\hspace{0pt} solves problem when \par\marginsec{} come together
+        *text = text.replace("%EndSubSubSection%", "}");
+        *text = text.replace("%para%", "\n\\par\n");
         //\hspace*{\fill}: https://tex.stackexchange.com/questions/54040/underful-hbox-badness-10000
-        latex.push_str(format!(r"{}\hspace*{{\fill}}\n\end{{spacing}}\n", text).as_str());
+        latex.push_str(format!("{}\\hspace*{{\\fill}}\n\\end{{spacing}}\n", text).as_str());
     }
 
     
@@ -213,17 +217,17 @@ fn mapply_templates(title:&str, text:&mut String, glosses:&Vec<Gloss>, header:&s
     // }
     
 
-    latex.push_str(r"\begin{table}[b!]\leftskip -0.84cm\n");
-    latex.push_str(r"\begin{tabular}{ m{0.2cm} L{3.25in} D{3.1in} }\n");
+    latex.push_str("\\begin{table}[b!]\\leftskip -0.84cm\n");
+    latex.push_str("\\begin{tabular}{ m{0.2cm} L{3.25in} D{3.1in} }\n");
     for g in glosses {
         let arrow = if g.arrow { "\textbf{→}" } else { "" };
         let mut lemma = g.lemma.replace('\\', "\\textbackslash");
-        lemma = lemma.replace('&', r"\&");
+        lemma = lemma.replace('&', "\\&");
         
-        lemma = lemma.replace("<i>", "\textit{");
+        lemma = lemma.replace("<i>", "\\textit{");
         lemma = lemma.replace("</i>", "}");
         let mut def = g.def.replace('\\', "\\textbackslash");
-        def = def.replace('&', r"\&");
+        def = def.replace('&', "\\&");
         
         def = def.replace("<i>", "\textit{");
         def = def.replace("</i>", "}");
@@ -231,8 +235,8 @@ fn mapply_templates(title:&str, text:&mut String, glosses:&Vec<Gloss>, header:&s
         latex.push_str(format!("{} & {} & {} \\\\\n", arrow, lemma, def).as_str());
         //$arrow . ' & ' . $lemma . ' & ' . $def . ' \\\\' . "\n";
     }
-    latex.push_str(r"\end{tabular}\n");
-    latex.push_str(r"\end{table}\n");
+    latex.push_str("\\end{tabular}\n");
+    latex.push_str("\\end{table}\n");
 
     latex = latex.replace(r"& Α´", "Α´");//hack to fix two type 2s together, lines 866
     latex = latex.replace(r"& Β´", "Β´");//hack to fix two type 2s together, lines 872
