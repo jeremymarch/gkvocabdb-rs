0f991076d310c8d61594339dcd10b2d69ab84b5c
jeremymarch <jmarch@gradcenter.cuny.edu>
2024-11-20 23:34:12 -0500

    add jest tests

---
+++ index.test.js
@@ -0,0 +1,14 @@
+// eslint-disable-next-line no-unused-vars
+/* global require expect test describe */
+// import * as blah from "scripts/index.js";
+// const mod = require('./static/index.html');
+
+function add (a, b) {
+  return a + b;
+}
+
+describe('Test Testing', () => {
+  test('1 + 2 === 3', () => {
+    expect(add(1, 2)).toBe(3);
+  });
+});
